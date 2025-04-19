e9aa131077fd66bc056168e0ecbd693fb91f1f74
SAFAL TIWARI <safaltiwari602@gmail.com>
2025-04-20 01:45:05 +0530

    Add files via upload

---
+++ stu_server.cpp
@@ -0,0 +1,39 @@
+#include <emscripten/bind.h>
+#include <unordered_map>
+#include <string>
+
+// Structure to store student details
+struct Student {
+    std::string fullName;
+    std::string branch;
+    std::string category;
+    std::string roomNumber;
+    int numberOfDays; // Number of days the student stayed
+    std::string month; // Month for which payment is being made
+};
+
+// Database of student details
+std::unordered_map<std::string, Student> studentDatabase = {
+    {"35130", {"Safal Tiwari", "IoT", "Gen", "R-5", 30, "April"}},
+    {"34605", {"Sumit Rajpoot", "IoT", "Obc", "TV-Room", 25, "April"}},
+    {"35193", {"Mayank Jaiswal", "IoT", "Obc", "29", 28, "April"}}
+};
+
+// Function to retrieve student details based on scholar number
+std::string getStudentDetails(const std::string& scholarNumber) {
+    if (studentDatabase.find(scholarNumber) != studentDatabase.end()) {
+        const Student& student = studentDatabase[scholarNumber];
+        return "Full Name: " + student.fullName + "\n" +
+               "Branch: " + student.branch + "\n" +
+               "Category: " + student.category + "\n" +
+               "Room Number: " + student.roomNumber + "\n" +
+               "Number of Days: " + std::to_string(student.numberOfDays) + "\n" +
+               "Month: " + student.month;
+    }
+    return "Error: Scholar number not found!";
+}
+
+// Bind the C++ function to make it callable from JavaScript
+EMSCRIPTEN_BINDINGS(student_server_module) {
+    emscripten::function("getStudentDetails", &getStudentDetails);
+}
